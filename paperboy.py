91fe514e9251f321725fe590bbe0f0ad6a38232a
Anton <anton.mois33v@gmail.com>
2019-02-15 17:52:56 -0500

    Completed Paperboy class

--- paperboy.py
+++ paperboy.py
@@ -1,5 +1,3 @@
-import math
-
 class Paperboy:
 
     """ A class representing the main character in a 1985 arcade game called "Paperboy" """
@@ -13,16 +11,19 @@ class Paperboy:
         return "Paperboy: {}".format(self.name)
 
     def quota(self):
-        # might have to round this to an integer
-        return 50 + (self.experience/2)
+        return 50 + (self.experience//2)
 
-    def deliver(self, start_address, end address):
+    def deliver(self, start_address, end_address):
         current_quota = self.quota()
         n_houses = abs(end_address - start_address) + 1
         if n_houses >= current_quota:
-            self.earnings = 0.25 * (current_quota + 2 * (n_houses - current_quota))
+            self.earnings += 0.25 * (current_quota + 2 * (n_houses - current_quota))
         else:
-            self.earnings = 0.25 * n_houses - 2
+            self.earnings += 0.25 * n_houses - 2
+        self.experience += n_houses
+
+    def report(self):
+        return "I'm {}, I've delivered {} papers and I've earned ${:.2f} so far!".format(self.name, self.experience, self.earnings)
 
 
 # instantiating the Paperboy class (creating an object of class Paperboy)
@@ -30,3 +31,16 @@ tommy = Paperboy("Tommy")
 print(tommy)
 # checking the quota for the day
 print(tommy.quota())
+# deliver to 60 houses today
+tommy.deliver(101, 160)
+print(tommy.earnings)
+# report the status
+print(tommy.report())
+
+# check quota again
+print(tommy.quota())
+# deliver to 75 houses
+tommy.deliver(1,75)
+print(tommy.earnings)
+# report the status
+print(tommy.report())
